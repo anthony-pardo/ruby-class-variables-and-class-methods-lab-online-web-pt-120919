cb6d55123e3c244a397a124c426c099738d9d83c
Anthony Pardo <pardo.anthony@gmail.com>
2020-01-09 22:25:32 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -3,5 +3,14 @@ class song
   
   def initialize(name, artist, genre)
     
+    @name = name
+    @artist = artist 
+    @genre = genre
     
+    @@count += 1 
+  end
+  
+  @@count = 0 
+  
+  def self.count
 end
